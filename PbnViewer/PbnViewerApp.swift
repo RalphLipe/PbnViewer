f5300cd661d4657204cfeb8b9121cbb4400e1587
Ralph Lipe <ralph@lipe.ws>
2023-04-25 12:18:16 -0700

    Initial Commit

---
+++ PbnViewer/PbnViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbnViewerApp.swift
+//  PbnViewer
+//
+//  Created by Ralph Lipe on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PbnViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
